d0929b92f768210a03c2501044e47363702c82a4
Sidorov <sidorov.bmstu@gmail.com>
2022-12-20 11:50:21 +0300

    new chart bars

--- guess_number.py
+++ guess_number.py
@@ -9,34 +9,6 @@ is more or less than the one we made.
 Thanks to game rules Computer has unlimited tries so it has no chance to loose. 
 We just want to know how many tries Computer will take.
 """
-def print_tabs(*args, tabs: tuple=None):
-    for arg, tab in zip(args, tabs):
-        print(arg, ' '*(tab-len(str(arg))), sep='', end='')
-    print()
-
-def average_int_result(func, repeat: int=1000, print_chart: bool=True):
-    def dec_func(*args, **kwargs):
-        results = [func(*args, **kwargs) for i in range(repeat)]
-        avrg_res = round(mean(results))
-        
-        if print_chart:
-            # Printing a simple chart
-            min_res = min(results) # lower bound of chart value axis
-            max_res = max(results) # upper bound of chart value axis
-            # Creating a dictionary:
-            # keys are results returned by func();
-            # values are numbers of times each result was returned.
-            results = {i: results.count(i) 
-                        for i in range(min_res, max_res+1)}
-            max_val = max(results.values()) # count of the most frequent result
-            
-            for result in results.items():
-                chart_ch = '=' if result[0] == avrg_res else '-'
-                chart_bar = chart_ch * round(result[1]/max_val*50)
-                print_tabs(result[0], '-', result[1], chart_bar, tabs=(3,2,5,0))
-                
-        return avrg_res
-    return dec_func
 
 def guess_number(lo: int=1, hi: int=100, number: int=None, bnr_srch: bool=True) -> int:
     """
@@ -88,8 +60,37 @@ def guess_number(lo: int=1, hi: int=100, number: int=None, bnr_srch: bool=True) 
         count += 1
 
 
-#if __name__ == '__main__':
-''' guess_number = average_int_result(guess_number, repeat=100)
-    for hi in [10**i for i in range(2,6)]:
+if __name__ == '__main__':
+
+    from math import ceil
+
+    def average_int_result(func, repeat: int=1000, print_chart: bool=True, bar_len: int=35):
+        def dec_func(*args, **kwargs):
+            results = [func(*args, **kwargs) for i in range(repeat)]
+            avrg_res = round(mean(results))
+            
+            if print_chart:
+                # Printing a simple chart
+                min_res = min(results) # lower bound of chart value axis
+                max_res = max(results) # upper bound of chart value axis
+                # Creating a dictionary:
+                # keys are results returned by func();
+                # values are numbers of times each result was returned.
+                results = {i: results.count(i) 
+                            for i in range(min_res, max_res+1)}
+                max_val = max(results.values()) # count of the most frequent result
+                
+                for result in results.items():
+                    if result[1]:
+                        chart_ch = '=' if result[0] == avrg_res else '-'
+                        indent = ' '*(len(str(max_res)) - len(str(result[0])))
+                        chart_bar = indent + chart_ch * ceil(result[1]/max_val * bar_len)
+                        print(result[0], chart_bar, f"{result[1]} times")
+                    
+            return avrg_res
+        return dec_func
+    
+    guess_number = average_int_result(guess_number)
+    for hi in [10**i for i in range(1,6)]:
         print(f"\n[1; {hi}]")
-        print(f"Average number of tries is {guess_number(1, hi)}")'''
+        print(f"Average number of tries is {guess_number(1, hi)}")
